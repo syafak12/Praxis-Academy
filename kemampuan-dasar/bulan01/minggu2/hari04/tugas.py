28e7c4c6b83cf96ced79b6dbf4b44e3b70a3c73b
syafak12 <kapasansyafak@gmail.com>
2022-04-25 15:43:17 +0700

    update

--- kemampuan-dasar/bulan01/minggu2/hari04/tugas.py
+++ kemampuan-dasar/bulan01/minggu2/hari04/tugas.py
@@ -1,63 +1,61 @@
-# class Api:
-#     def fetch_remote_data(self):
-#         print('Api called')
-#         return 42
+from injector import Injector, SingletonScope, provider
 
 
-# class BusinessLogic:
-#     def __init__(self, api: Api):
-#         self.api = api
-
-#     def do_stuff(self):
-#         api_result = self.api.fetch_remote_data()
-#         print(f'the api returned a result: {api_result}')
-#         # do something with the data and return a result
-
-# # # ---
-
-# if __name__ == '__main__':
-#     api = Api()
-#     logic = BusinessLogic(api=api)
+class Api:
+    def fetch_remote_data(self):
+        print('Api called')
+        return 42
 
 
+class BusinessLogic:
+    def __init__(self, api: Api):
+        self.api = api
 
+    def do_stuff(self):
+        api_result = self.api.fetch_remote_data()
+        print(f'the api returned a result: {api_result}')
+        # do something with the data and return a result
 
-# class Api:
-#     def fetch_remote_data(self):
-#         print('Api called')
-#         return 42
+# ---
 
+if __name__ == '__main__':
+    api = Api()
+    logic = BusinessLogic(api=api)
 
-# class BusinessLogic:
-#     def __init__(self, api: Api):
-#         self.api = api
-
-#     def do_stuff(self):
-#         api_result = self.api.fetch_remote_data()
-#         print(f'the api returned a result: {api_result}')
-#         # do something with the data and return a result
+    # ...
+    print(logic.do_stuff())
 
 
 
-#     # ...
-#     print(logic.do_stuff())
+class Api:
+    def fetch_remote_data(self):
+        print('Api called')
+        return 42
+class BusinessLogic:
+    def __init__(self, api: Api):
+        self.api = api
+
+    def do_stuff(self):
+        api_result = self.api.fetch_remote_data()
+        print(f'the api returned a result: {api_result}')
+        # do something with the data and return a result
+print(logic.do_stuff())
 
 
 
+class AppModule(Module):SingletonScope
+    # @provide
 
-class AppModule(Module):
-
-    @singleton
-    @provider
+    # @singleton
+    # @provider
     def provide_business_logic(self, api: Api) -> BusinessLogic:
         return BusinessLogic(api=api)
 
-    @singleton
-    @provider
+    # @singleton
+    # @provider
     def provide_api(self) -> Api:
         # there is no complex logic in our case,
-        # but you can use this method to hide the complexity of initial 
-        configuration
+        # but you can use this method to hide the complexity of initial configuration
         # e.g. when instantiating a particular DB connector.
         return Api()
 
@@ -66,3 +64,24 @@ if __name__ == '__main__':
 
     logic = injector.get(BusinessLogic)
     logic.do_stuff()
+
+
+class TestApi(Api):
+    def fetch_remote_data(self):
+        print('Demo Api called')
+        return 24
+class TestAppModule(Module):
+
+    @SingletonScope
+    @provider
+    def provide_api(self) -> Api:
+        return TestApi()
+if __name__ == '__main__':
+    real_injector = Injector(AppModule())
+    test_injector = Injector([AppModule(), TestAppModule()])
+
+    real_logic = real_injector.get(BusinessLogic)
+    real_logic.do_stuff()
+
+    test_logic = test_injector.get(BusinessLogic)
+    test_logic.do_stuff()
