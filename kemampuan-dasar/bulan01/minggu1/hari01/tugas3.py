3fa7580272046ca50500b5250a2dcfcfa8cc7c03
syafak12 <kapasansyafak@gmail.com>
2022-05-15 20:54:23 +0700

    update

--- kemampuan-dasar/bulan01/minggu1/hari01/tugas3.py
+++ kemampuan-dasar/bulan01/minggu1/hari01/tugas3.py
@@ -1,142 +1,142 @@
-# kita = 32
-# mereka = 3 * 4
-# print(kita*mereka)
+kita = 32
+mereka = 3 * 4
+print(kita*mereka)
 
-# syafak = 12
-# doni = 12/4
-# print(syafak+doni)
+syafak = 12
+doni = 12/4
+print(syafak+doni)
 
-# ike = 6
-# adi = 2-1
-# print(ike*adi)
+ike = 6
+adi = 2-1
+print(ike*adi)
 
-# s = 40
-# k = 12 * 4
-# print(s * k)
+s = 40
+k = 12 * 4
+print(s * k)
 
-# aku = 3
-# kamu = 5
-# print(aku * kamu)
-
-
-
-# e = 5;
-# y = 6;
-# k = 9;
-# b = 7;
-# c = 62
-# if e-y+k*b == c:
-#     print("benar")
-# elif e-y+k*b == y:
-#     print("salah")
-
-# syafak = 3;
-# doni = 2;
-# adi = 3;
-# ike = 4;
-# ika = 6;
-# hanif = 3
-# if syafak+doni-adi*ike/ika == hanif:
-#     print("benar")
-# elif syafak+doni-adi*ike/ika == ika:
-#     print("salah")
-
-# a = "udin";
-# b = "syaffak";
-# if a == "udin":
-#     print("benar")
-# else:
-#     print("salah")
+aku = 3
+kamu = 5
+print(aku * kamu)
 
 
-# tax = 12.5 / 100
-# price = 100.50
-# print(price*tax)
+
+e = 5;
+y = 6;
+k = 9;
+b = 7;
+c = 62
+if e-y+k*b == c:
+    print("benar")
+elif e-y+k*b == y:
+    print("salah")
+
+syafak = 3;
+doni = 2;
+adi = 3;
+ike = 4;
+ika = 6;
+hanif = 3
+if syafak+doni-adi*ike/ika == hanif:
+    print("benar")
+elif syafak+doni-adi*ike/ika == ika:
+    print("salah")
+
+a = "udin";
+b = "syaffak";
+if a == "udin":
+    print("benar")
+else:
+    print("salah")
 
 
-# a = "Py"
-# print(a + "thon")
-
-# s = "sya"
-# print(s + "fak")
+tax = 12.5 / 100
+price = 100.50
+print(price*tax)
 
 
-# a = "aku_disini"
-# print(a + "praxis-academy")
+a = "Py"
+print(a + "thon")
 
-# b = "aku_ma"
-# print(b + "lu_disini")
-
-# word = "python"
-# print(word[0])
-# print(word[5])
-# print(word[-1])
-# print(word[-2])
-# print(word[-6])
-# print(word[:2])
-# print(word[:5])
-# print(word[:2])
-# print(word[4:])
-# print(word[-2:])
-# print(word[1:5] + word[4:2])
-# print(word[1:4] + word[4:1])
+s = "sya"
+print(s + "fak")
 
 
-# kata = "syafak";
-# print(kata[1:])
-# print("m" + kata[1:])
+a = "aku_disini"
+print(a + "praxis-academy")
 
-# kata = "python"
-# print(kata[1:])
+b = "aku_ma"
+print(b + "lu_disini")
 
-# kata = "python"
-# print(kata[:2])
-
-# kata = "python"
-# print(kata[2:])
-
-
-# # menghitung jumlah huruf di dalam teks
-# s = "supercalifragilisticexpialidocious"
-# print(len(s))
-
-
-# a = 4;
-# b = 3;
-# print(a ** b)
+word = "python"
+print(word[0])
+print(word[5])
+print(word[-1])
+print(word[-2])
+print(word[-6])
+print(word[:2])
+print(word[:5])
+print(word[:2])
+print(word[4:])
+print(word[-2:])
+print(word[1:5] + word[4:2])
+print(word[1:4] + word[4:1])
 
 
-# kami = ['a', 'b', 'c', 'd', 'e',]
-# kami = 4;
-# kami = 2;
-# print("kami")
+kata = "syafak";
+print(kata[1:])
+print("m" + kata[1:])
+
+kata = "python"
+print(kata[1:])
+
+kata = "python"
+print(kata[:2])
+
+kata = "python"
+print(kata[2:])
 
 
-# from re import A
+# menghitung jumlah huruf di dalam teks
+s = "supercalifragilisticexpialidocious"
+print(len(s))
 
 
-# kami = ['a', 'b', 'c', 'd', 'e',]
-# print(len ("kami"))
+a = 4;
+b = 3;
+print(a ** b)
 
 
-# a = "saya mau masuk ke ruangan pribadi"
-# print(len(a))
+kami = ['a', 'b', 'c', 'd', 'e',]
+kami = 4;
+kami = 2;
+print("kami")
 
 
-# i = 256*256
-# print('The value of i is', i)
-# a, b = 0, 1
-# while a < 1000:
-#     print(a, end=',')
-#     a, b = b, a+b
+from re import A
 
 
-# i = 256*256
-# print('Nilai i adalah', i)
-# a, b = 0, 1
-# while a < 1000:
-#     print(a, end=',')
-#     a, b = b, a+b
+kami = ['a', 'b', 'c', 'd', 'e',]
+print(len ("kami"))
+
+
+a = "saya mau masuk ke ruangan pribadi"
+print(len(a))
+
+
+i = 256*256
+print('The value of i is', i)
+a, b = 0, 1
+while a < 1000:
+    print(a, end=',')
+    a, b = b, a+b
+
+
+i = 256*256
+print('Nilai i adalah', i)
+a, b = 0, 1
+while a < 1000:
+    print(a, end=',')
+    a, b = b, a+b
 
 
 
