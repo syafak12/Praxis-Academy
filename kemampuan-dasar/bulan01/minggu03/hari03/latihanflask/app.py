e6fd9ecfa7add5200f9429a00241020769887bd9
syafak12 <kapasansyafak@gmail.com>
2022-05-23 12:12:06 +0700

    update

--- kemampuan-dasar/bulan01/minggu03/hari03/latihanflask/app.py
+++ kemampuan-dasar/bulan01/minggu03/hari03/latihanflask/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/profil')
 def hello():
     data = {
         "id": 1,
@@ -14,6 +14,34 @@ def hello():
             "kapasan"
         ],
     }
-    return data
+
+@app.route('/profil_satu')
+def datane_doni():
+    data_satu = {
+        "id": 1,
+        "nama": "doni",
+        "lahir": True,
+        "kota": [
+            "jogja",
+            "sumber",
+            "banyurowo"
+        ],
+    }
+
+@app.route('/profil_dua')
+def datane_adi():
+    data_dua = {
+        "id": 1,
+        "nama": "adi",
+        "lahir": True,
+        "kota": [
+            "pati",
+            "tayu",
+            "ndeso"
+        ],
+    }
+    return ("profil", 
+    "profil_satu", 
+    "profil_dua")
 
     
