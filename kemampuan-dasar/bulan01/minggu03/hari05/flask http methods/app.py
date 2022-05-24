fbe65dfbd4de1ad57696c1f065e2e54dccd1024c
syafak12 <kapasansyafak@gmail.com>
2022-05-24 13:54:54 +0700

    update

---
+++ kemampuan-dasar/bulan01/minggu03/hari05/flask http methods/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route('/success/<name>/<alamat>')
+def success(name,alamat):
+   return f"welcome {name} alamatmu {alamat}"
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      alamat = request.form['alamat']
+      return redirect(url_for('success',name = user,alamat= alamat))
+   else:
+      user = request.args.get('nm')
+      alamat = request.args.get('alamat')
+      return redirect(url_for('success',name = user,alamat= alamat))
+
+if __name__ == '__main__':
+   app.run(debug = True)
