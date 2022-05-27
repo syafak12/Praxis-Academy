2cc655f487be8a58f266a265d6ca10598ef6df12
syafak12 <kapasansyafak@gmail.com>
2022-05-27 11:22:45 +0700

    update

---
+++ kemampuan-dasar/bulan01/minggu03/hari05/flask_static _file/static/hello.js
@@ -0,0 +1,9 @@
+function sayHello() {
+    alert("Hello World")
+ }
+
+// function sayHello()
+// {
+//    var a=document.getElementById("div_semangat");
+//    a.innerHTML+="<p>Sedang Belajar JavaScript, Semangat...!!!</p>";
+// }
