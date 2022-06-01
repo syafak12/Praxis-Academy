75ca09f2fc44f331f03a90617c9b18885538626c
syafak12 <kapasansyafak@gmail.com>
2022-06-01 23:38:14 +0700

    update

---
+++ kemampuan-dasar/bulan01/minggu04/youtube_project/server.py
@@ -0,0 +1,43 @@
+from flask import Flask, Response
+import pymongo
+import json
+app = Flask(__name__)
+
+try:
+    mongo = pymongo.MongoClient(
+        host="localhost",
+        port=27017,
+        serverSelectionTimoutMS = 1000  # memicu pengecualian jika tidak bisa ke db
+    )
+    db = mongo.company
+except:
+    print ("ERROR - Cannot connect to db")
+
+####################
+@app.route('/users', methods=["POST"])
+def create_user():
+    try:
+        user = {"name": "A", "lastName": "AA"}
+        dbResponse = db.users.insert_one(user)
+        print(dbResponse.inserted_id)
+        # for attr in dir(dbResponse):
+        #     print(attr)
+        return Response(
+            response= json.dums(
+                {"message": "user created", 
+                "id":f"{dbResponse.inserted_id}" 
+                }),
+            status=200,
+            mimetype="application/json"
+        )
+
+    except Exception as ex:
+        print("********")
+        print(ex)
+        print("********")
+###################
+
+
+
+if __name__ == "__main__":
+    app.run()
