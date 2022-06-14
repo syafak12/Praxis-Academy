a565450f80b92606b87847865e4e1e09cea401fe
syafak12 <kapasansyafak@gmail.com>
2022-06-14 20:08:11 +0700

    update

--- kemampuan-dasar/bulan01/minggu04/database alumni/server.py
+++ kemampuan-dasar/bulan01/minggu04/database alumni/server.py
@@ -4,114 +4,24 @@ import json
 from bson.objectid import ObjectId
 app = Flask(__name__)
 
-
 try:
-    mongo = pymongo.MongoClient(
-        host="localhost",
+    mongo = pymongo.MOngoClient(
+        host-"localhost",
         port=27017,
-        serverSelectionTimoutMS = 1000  
+        serverSelectionTimeoutMS=1000
     )
-    db = mongo.company
-    mongo.server_info() # memicu pengecualian jika tidak bisa ke db
-except:
-    print ("ERROR - Cannot connect to db")
-####################
 
-@app.route("/users", methods=["GET"])
+    db = mongo.database alumni
+    mongo.server_info()
+    except:
+        print("MAAF - tidak connect ke db")
+
+#############
+### membaca data (read)
+############
+@app.route("/read", methods=["GET"])
 def get_some_users():
     try:
-        data = list(db.users.find())
+        data = list(db.data_alumni.find())
         for user in data:
-            user["_id"] = str(user["_id"])
-        return Response(
-            response= json.dumps(data),
-            status=500,
-            mimetype="application/json"
-            )
-    except Exception as ex:
-        print(ex)
-        return Response(response= json.dums({"message": "cannot read users"}), status=500, mimetype="application/json")
-
-
-####################
-@app.route("/users", methods=["POST"])
-def create_user():
-    try:
-        user = {
-            "name":request.form["name"], 
-            "lastName":request.form["lastName"]
-        }
-        dbResponse = db.users.insert_one(user)
-        print(dbResponse.inserted_id)
-        # for attr in dir(dbResponse):
-        #     print(attr)
-        return Response(
-            response= json.dums(
-                {"message": "user created", 
-                "id":f"{dbResponse.inserted_id}" 
-                }),
-            status=200,
-            mimetype="application/json"
-        )
-    except Exception as ex:
-        print("********")
-        print(ex)
-        print("********")
-###################
-@app.route("/users/<id>", methods=["PATCH"])
-def update_user(id):
-    try:
-        dbResponse = db.users.update_one(
-            {"_id":ObjectId(id)},
-            {"$set":{"name":request.form["name"]}}
-        )
-        # for attr ini dir(dbRensponse):
-        #     print(f"*****{attr}*****")
-        if dbResponse.modified_count == 1:
-            return Response(
-                response= json.dums(
-                    {"message": "user updated"}),
-                status=200,
-                mimetype="application/json"
-            )
-        return Response(
-            response= json.dums(
-                {"message": "nothing to update"}),
-            status=200,
-            mimetype="application/json"
-        )
-    except Exception as ex:
-        print("******************")
-        print(ex)
-        print("******************")
-        return Response(
-            response= json.dums(
-                {"message": "sorry cannot update user"}),
-            status=500,
-            mimetype="application/json"
-        )
-###################
-@app.route("/users/<id>", methods=["DELETE"])
-def delete_user(id):
-    try:
-        dbResponse = db.users.delete_one({"_id":ObjectId(id)})
-        if dbResponse.deleted_count == 1:
-            return Response(response= json.dums({"message": "cannot delete user", "id":f"{id}"}), status=200, mimetype="application/json")
-        return Response(response= json.dums({"message": "cannot delete user", "id":f"{id}"}), status=200, mimetype="application/json")
-    except Exception as ex:
-        print("******")
-        print(ex)
-        print("******")
-        return Response(
-            response= json.dums(
-                {"message": "sorry cannot delete user"}),
-            status=500,
-            mimetype="application/json"
-        )
-
-###################
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+            user["_id"] = str(user["__id"])
