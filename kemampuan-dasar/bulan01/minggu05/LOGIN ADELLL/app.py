4f36873d829549379df25e409b362a33eb0151ff
syafak12 <kapasansyafak@gmail.com>
2022-06-16 17:15:37 +0700

    update

--- kemampuan-dasar/bulan01/minggu05/LOGIN ADELLL/app.py
+++ kemampuan-dasar/bulan01/minggu05/LOGIN ADELLL/app.py
@@ -1,51 +1,43 @@
 from flask import Flask, request, session, redirect, url_for, render_template, flash
 import psycopg2 #pip install psycopg2 
 import psycopg2.extras
+import re 
 from werkzeug.security import generate_password_hash, check_password_hash
  
 app = Flask(__name__)
-app.secret_key = 'lupasandi46'
+app.secret_key = 'cairocoders-ednalan'
  
 DB_HOST = "localhost"
-DB_NAME = "login"
-DB_USER = "login"
-DB_PASS = "lupasandi"
+DB_NAME = "sampledb"
+DB_USER = "postgres"
+DB_PASS = "admin"
  
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
  
 @app.route('/')
 def home():
     # Check if user is loggedin
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    s = "SELECT * FROM users"
-    cur.execute(s)
-    list_users = cur.fetchall()
-    # if 'loggedin' in session:
+    if 'loggedin' in session:
     
-    #     # User is loggedin show them the home page
-    #     return render_template('home.html', username=session['username'])
-    # # User is not loggedin redirect to login page
-    # return redirect(url_for('login'))
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
  
-@app.route('/login/', methods=['POST'])
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
    
     # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST':
-        fullname = request.form['fullname']
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
-        email = request.form['email']
-        cur.execute("INSERT INTO tb_users (fullname, username, password, email) VALUES (%s, %s, $s, %s)" (fullname, username, password, email))
-        conn.commit()
-        flash('ok')
-        
+        print(password)
  
         # Check if account exists using MySQL
-        cur.execute('SELECT * FROM users WHERE username = %s', (username,))
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
         # Fetch one record and return result
-        account = cur.fetchone()
+        account = cursor.fetchone()
  
         if account:
             password_rs = account['password']
@@ -69,20 +61,20 @@ def login():
   
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
  
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    # if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
         # Create variables for easy access
-        # fullname = request.form['fullname']
-        # username = request.form['username']
-        # password = request.form['password']
-        # email = request.form['email']
+        fullname = request.form['fullname']
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
     
-        # _hashed_password = generate_password_hash(password)
+        _hashed_password = generate_password_hash(password)
  
         #Check if account exists using MySQL
-        cur.execute('SELECT * FROM users WHERE username = %s', (username,))
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
         account = cursor.fetchone()
         print(account)
         # If account exists show error and validation checks
