16288a17beca6612fdafd661f2b03f39380f384e
syafak12 <kapasansyafak@gmail.com>
2022-06-22 14:04:02 +0700

    upudate

--- kemampuan-dasar/bulan01/minggu05/cronos accounting & finance/server.py
+++ kemampuan-dasar/bulan01/minggu05/cronos accounting & finance/server.py
@@ -43,10 +43,64 @@ def create():
             "message": "sukses",
             "data": upload
         })
+    #     payload = json.loads(request.data)
+    #     # print(payload)
+
+    #     upload = payload["upload"]
+    #     query = f"insert into features"
+    #     query = 'insert into features (upload) values ("%s")'%("upload")
+    #     query = f"into features set upload=('{upload}')"
+    #     print(query)
+    #     curs.execute(query)
+    #     conn.commit()
+    #     conn.close
+    #     curs.close
+    #     result = curs.fetchall()
+    #     print(result)
+    #     return jsonify({
+    #         "message": "sukses",
+    #         "data": []
+    #     })
     except Exception as e:
         return jsonify({
-            "message": "Gagal memasukkan data"
+            "message": "Gagal memasukkan data",
+            "error" : f"{e}"
         }), 400
+@app.route("/delete/<id>", methods=["DELETE"])
+def delete(id):
+    try:
+        # print(id)
+        query = f"delete from features a where a.id={id}"
+        curs.execute(query)
+        conn.commit()
+        return jsonify({
+            "message": "sukses",
+        })
+    except Exception as e:
+        return jsonify({
+            "message": "Gagal menghapus data",
+            "detailMessage": f"{e}"
+        }), 400
+
+@app.route("/update/<id>", methods=["PUT"])
+def update(id):
+    try:
+        payload = json.loads(request.data)
+        print(payload)
+        upload = payload["upload"]
+
+        query = f"update features set upload=('{upload}') where id=({id})"
+        curs.execute(query)
+        conn.commit()
+        return jsonify({
+            "message": "sukses"
+        })
+    except Exception as e:
+        return jsonify({
+            "message": "Gagal update data",
+            "detailMessage": f"{e}"
+        }), 400
+# @app.route
 
 if "__name__"=="__main__":
     app.run(host="0.0.0.0", port=8080)
