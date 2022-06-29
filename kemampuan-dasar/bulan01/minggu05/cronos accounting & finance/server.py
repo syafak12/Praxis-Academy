860d20a7b75b308f134a2471750fb21f736146d7
syafak12 <kapasansyafak@gmail.com>
2022-06-29 21:35:32 +0700

    update

--- kemampuan-dasar/bulan01/minggu05/cronos accounting & finance/server.py
+++ kemampuan-dasar/bulan01/minggu05/cronos accounting & finance/server.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask, flash, jsonify, redirect, request, json, url_for
 import psycopg2
 from flask_cors import CORS
-from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 CORS(app)
@@ -17,8 +16,6 @@ def baca():
         query = f"select * from features"
         curs.execute(query)
         result = curs.fetchall()
-        # print(result)
-
         data = []
         for i in result:
             data.append({
@@ -31,6 +28,26 @@ def baca():
     except Exception as e:
         print(e)
     
+@app.route("/read", methods=["GET"])
+def read():
+    try:
+        query = f"select * from content"
+        curs.execute(query)
+        result = curs.fetchall()
+        data = []
+        for i in result:
+            data.append({
+                "id": i[0],
+                "id_features": i[1],
+                "isi_content": i[2],
+                "image": i[3]
+            })
+        return jsonify({
+            "dtaa": data
+        })
+    except Exception as i:
+        print (i)
+
 @app.route("/membuat", methods=["POST"])
 def create():
     try:
@@ -48,29 +65,35 @@ def create():
             "message": "sukses membuat data",
             "data": "nama"
         })
-    #     payload = json.loads(request.data)
-    #     # print(payload)
-
-    #     upload = payload["upload"]
-    #     query = f"insert into features"
-    #     query = 'insert into features (upload) values ("%s")'%("upload")
-    #     query = f"into features set upload=('{upload}')"
-    #     print(query)
-    #     curs.execute(query)
-    #     conn.commit()
-    #     conn.close
-    #     curs.close
-    #     result = curs.fetchall()
-    #     print(result)
-    #     return jsonify({
-    #         "message": "sukses",
-    #         "data": []
-    #     })
+    
     except Exception as e:
         return jsonify({
             "message": "Gagal memasukkan data",
             "error" : f"{e}"
         }), 400
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    try:
+        payload = json.loads(request.data)
+        id_features = payload["id_features"]
+        isi_content = payload["isi_content"]
+        image = payload["image"]
+        query = f" insert into content (id_features), (isi_content), (image) values ('{id_features}'), ('{isi_content}'), ('{image}')"
+        curs.execute(query)
+        conn.commit()
+        return jsonify({
+            "message": "sukses memasukkan data content",
+            "data": "id_features"
+        }), 200
+
+    except Exception as i:
+        return jsonify({
+            "message": "gagal memasukkan data content",
+            "error": f"{i}"
+        }), 400
+
+
 @app.route("/hapus/<id_menu>", methods=["DELETE"])
 def hapus(id_menu):
     try:
@@ -86,6 +109,21 @@ def hapus(id_menu):
             "message": "Gagal menghapus data",
             "detailMessage":f"{e}"
         }),400
+
+@app.route("/delete/<id>", methods=["DELETE"])
+def delete(id):
+    try:
+        query = f"delete from content where id ={id}"
+        curs.execute(query)
+        conn.commit()
+        return jsonify({
+            "message": "sukses menghapus data"
+        }), 200
+    except Exception as i:
+        return jsonify({
+            "message": "gagal menghapus data",
+            "detailMessage": f"{i}"
+        }), 400
 
 @app.route("/update/<id_menu>", methods=["PUT"])
 def update(id_menu):
@@ -105,7 +143,26 @@ def update(id_menu):
             "message": "Gagal update data",
             "detailMessage": f"{e}"
         }), 400
-# @app.route
+
+@app.route("/perbaharui/<id>", methods=["PUT"])
+def perbaharui(id):
+    try:
+        payload = json.loads(request.data)
+        id_features = payload["id_features"]
+        isi_content = payload["isi_content"]
+        image = payload["image"]
+        query = f"update content (id_features), (isi_content), (image) values ('{id_features}'), ('{isi_content}'), ('{image}')"
+        curs.execute(query)
+        conn.commit()
+        return jsonify({
+            "message": "sukses memasukkan data content",
+            "data": "id_features"
+        }), 200
+    except Exception as i:
+        return jsonify({
+            "message": "Gagal update data",
+            "detailMessage": f"{i}"
+        }), 400
 
 if "__name__"=="__main__":
     app.run(host="0.0.0.0", port=8080)
